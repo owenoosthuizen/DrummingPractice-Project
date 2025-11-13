acdf4e5f53e0056cf5365c89b9f0f19751cfdcd4
Owen Oosthuizen <owenoosthuizen2001@gmail.com>
2025-11-13 11:42:51 +0000

    created data for sessions, exercises and sessions.

--- src/main/kotlin/Models/Exercise.kt
+++ src/main/kotlin/Models/Exercise.kt
@@ -2,7 +2,7 @@ package Models
 
 data class Exercise (
     val exerciseId: Int,
-    val drum: String,
+    val drumPart: String,
     val lengthInMins: Int,
     val difficultyLevel: String,
     val description: String
