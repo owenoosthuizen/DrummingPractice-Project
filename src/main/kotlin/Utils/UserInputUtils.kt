8753d6e40ea5c1d2c3357498ccd7e7946f058715
Owen <20111958@setu.ie>
2025-11-20 18:11:25 +0000

    Add simple controllers for sessions, exercises and sessionexercises

---
+++ src/main/kotlin/Utils/UserInputUtils.kt
@@ -0,0 +1,39 @@
+package Utils
+
+fun readUserInt(UserInt: String): Int{
+    while (true){
+        print(UserInt)
+        val input = readlnOrNull()
+        try{
+            return input!!.toInt()
+        }catch (e: Exception){
+            println("Please enter a valid number. ")
+        }
+    }
+}
+
+fun readUserDouble(UserD: String): Double{
+    while (true){
+        print(UserD)
+        val input = readlnOrNull()
+        try{
+            return input!!.toDouble()
+        }catch (e: Exception){
+            println("Please enter a valid decimal number.")
+        }
+    }
+}
+
+fun readUserBoolean(UserBoo: String): Boolean{
+    while (true){
+        print(UserBoo)
+        val input = readlnOrNull()
+        if (input == "true" || input == "false")return input.toBoolean()
+        println("Please type true OR false")
+    }
+}
+
+fun readUserString(UserPromt: String): String{
+    print(UserPromt)
+    return readln()
+}
