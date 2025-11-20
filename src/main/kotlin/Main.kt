3449cade605e5c7269519f178ea4cf22d3ab1605
Owen <20111958@setu.ie>
2025-11-20 18:38:06 +0000

    Add Main Menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,31 @@
-class Main {
+import Utils.*
+
+fun main() {
+
+    var choice: Int
+
+    do {
+        choice = readUserInt("""
+            |
+            |==================================
+            |        WEEKLY DRUM PRACTICE
+            |==================================
+            | 1. Add Session
+            | 2. Add Exercise
+            | 3. View Sessions
+            | 4. View Sessions
+            | 5. Link Exercise to Session
+            | 6. Mark Exercise Completed
+            | 7. View All Session Exercises
+            | 0. Exit
+            |----------------------------------
+            | > 
+        """.trimMargin())
+
+        when (choice) {
+
+            else -> println("Invalid choice. Try again.")
+        }
+
+    } while (choice != 0)
 }
