cf655611a0bc34d55da7396e52728ab2b20e891f
Owen <20111958@setu.ie>
2025-11-27 15:43:16 +0000

    Implementing XMLSerializer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "1.9.10"
+    application
 }
 
-group = "ie.setu"
+group = "ie.yourname"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,12 +11,18 @@ repositories {
 }
 
 dependencies {
+    // Kotlin test
     testImplementation(kotlin("test"))
+
+    // Logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+
+    // XML + JSON persistence
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
---
+++ src/main/kotlin/Persistence/XMLSerializer.kt
@@ -0,0 +1,33 @@
+package persistence
+
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.xml.StaxDriver
+import java.io.FileReader
+import java.io.FileWriter
+
+class XMLSerializer(private val file: String): Serializer {
+
+    private val xstream = XStream(StaxDriver())
+
+    init {
+        XStream.setupDefaultSecurity(xstream)
+        xstream.allowTypesByWildcard(arrayOf("Models.*", "Controllers.*"))
+    }
+
+    override fun write(obj: Any?) {
+        val writer = FileWriter(file)
+        xstream.toXML(obj, writer)
+        writer.close()
+    }
+
+    override fun read(): Any? {
+        return try {
+            val reader = FileReader(file)
+            val obj = xstream.fromXML(reader)
+            reader.close()
+            obj
+        } catch (_: Exception) {
+            null
+        }
+    }
+}
