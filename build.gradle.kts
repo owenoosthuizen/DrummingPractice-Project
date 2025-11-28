2183c7c4a87d8850301b383294698e9413396a4d
Owen Oosthuizen <owenoosthuizen2001@gmail.com>
2025-11-28 08:59:23 +0000

    small tweaks

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,8 @@ repositories {
 }
 
 dependencies {
-    // Kotlin test
     testImplementation(kotlin("test"))
 
-    // XML + JSON persistence
     implementation("com.thoughtworks.xstream:xstream:1.4.21")
     implementation("org.codehaus.jettison:jettison:1.5.4")
 }
@@ -23,10 +21,12 @@ kotlin {
     jvmToolchain(17)
 }
 
+application {
+    mainClass.set("MainKt")
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.test {
